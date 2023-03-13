8bf839db92a5d1b37300968e022a088292812491
LeeHS <wodlrr2@naver.com>
2023-03-13 23:17:59 +0900

    ㄹㅇㄹㅇㄹ

--- util/ServerPath.ts
+++ util/ServerPath.ts
@@ -1,3 +1,4 @@
-//export const IP = "http://1.237.1.88:8082/parking"
-export const IP_ = "http://10.0.2.2:8082"
-export const IP = "http://10.0.2.2:8082/parking"
+export const IP = "http://1.237.1.88:8082/parking"
+export const IP_ = "http://1.237.1.88:8082"
+//export const IP_ = "http://10.0.2.2:8082"
+//export const IP = "http://10.0.2.2:8082/parking"
